f35fafcb8e11ac7dbb246459b034bc7db99b6e9f
maaxizin25 <maaxipb25@gmail.com>
2023-10-26 00:30:03 -0300

    new-test

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -8,5 +8,8 @@ export const api = axios.create({
   },
 });
 export const apiActive = axios.create({
-  baseURL: "https://hooks.zapier.com/hooks/catch/16450525/38zbndc/",
+  baseURL: "https://hooks.zapier.com/hooks/catch/16450525/38zu6sv/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
