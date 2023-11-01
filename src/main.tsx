959c7d11b7f43f3e59aa727f0590192a843d5a4f
maaxizin25 <maaxipb25@gmail.com>
2023-11-01 01:00:11 -0300

    add: pixel

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyle } from "./styles/global.ts";
 import { Providers } from "./context/providers.tsx";
+import { MetaPixel } from "./components/metaPixel/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Providers>
       <GlobalStyle />
+      <MetaPixel />
       <App />
     </Providers>
   </React.StrictMode>
