a4b801e5d2c3cc25e381f0db7fb1acf3f85df911
maaxizin25 <maaxipb25@gmail.com>
2023-10-31 23:15:21 -0300

    add: LoadingButton

--- src/components/buttonSubmit/index.tsx
+++ src/components/buttonSubmit/index.tsx
@@ -1,9 +1,22 @@
+import { useContext } from "react";
 import { ButtonSubmitStyled } from "./style";
+import { AppContext } from "../../context/appContext";
 
 export const ButtonSubmitComponent = ({
   onClick,
 }: {
   onClick?: () => void;
 }) => {
-  return <ButtonSubmitStyled onClick={onClick}>Próximo</ButtonSubmitStyled>;
+  const { currentStep, loading } = useContext(AppContext);
+  return (
+    <ButtonSubmitStyled disabled={loading} onClick={onClick}>
+      {loading ? (
+        <div className="loader" />
+      ) : currentStep <= 5 ? (
+        "Próximo"
+      ) : (
+        "Horóscopo Abierto"
+      )}
+    </ButtonSubmitStyled>
+  );
 };
