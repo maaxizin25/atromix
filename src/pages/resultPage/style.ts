f1e6c370d2118087b2a9744e55e3d49fc1c8ee6e
maaxizin25 <maaxipb25@gmail.com>
2023-10-31 21:28:34 -0300

    fix: pc

--- src/pages/resultPage/style.ts
+++ src/pages/resultPage/style.ts
@@ -15,8 +15,14 @@ export const ResultPageStyle = styled.div`
   .container-box-horoscope {
     margin: 0 auto;
     background-color: white;
+    max-width: 710px;
     width: 95%;
     border-radius: 8px;
     padding: 20px;
+  }
+  @media (min-width: 600px) {
+    .container-box-horoscope {
+      padding: 60px;
+    }
   }
 `;
