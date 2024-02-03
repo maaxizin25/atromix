93d60611f59c9f146418f68b42f79b879fb45166
maaxizin25 <maaxipb25@gmail.com>
2024-02-03 12:23:32 -0300

    fix: geight

--- src/components/vslPage/style.ts
+++ src/components/vslPage/style.ts
@@ -3,26 +3,32 @@ import styled from "styled-components";
 export const VslPageStyle = styled.div`
   h2 {
     text-align: center;
-    font-size: 26px;
-  }
-  p {
-    text-align: center;
-    color: red;
-    margin-bottom: 10px;
+    font-size: 22px;
   }
   span {
     text-decoration: underline;
   }
   .video-vsl {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     padding: 150% 0 0 0;
     position: relative;
-    height: 100vh;
+    height: 600px;
   }
   iframe {
     position: absolute;
     top: 0;
-    left: 0;
-    width: 100%;
+    left: 50;
+    margin: 0 auto;
     height: 100%;
+  }
+  video {
+    width: 100px;
+  }
+  @media (min-width: 600px) {
+    iframe {
+      width: 100%;
+    }
   }
 `;
